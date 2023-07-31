99e524ee36e6a98dd6c96a7eae6fab115741f154
ale vel <ale.vrs@outlook.com>
2023-07-30 22:23:33 -0500

    Agregar datos de una API (GET)

---
+++ public/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCX9NJ471o7Wie1DQe94RVIg&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = document.querySelector('.content__show') || null;
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6c8aec95f0mshc835fd1a770d505p1250bfjsn6fdebd898161',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+	const response = await fetch(urlApi, options);
+	const data = await response.json();
+	return data;
+}
+
+(async () => {
+	try {
+		const videos = await fetchData(API);
+		let view = `
+			${videos.items.map(video => `
+				<article class="content__video">
+					<figure>
+						<img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" />
+					</figure>
+					<div>
+						<p>
+							${video.snippet.title}
+						</p>
+					</div>
+			</article>
+			`).slice(0, 4).join('')}
+		`;
+
+		content.innerHTML = view;
+	} catch (e) {
+		console.log(e);
+		console.warn(e);
+	}
+})();
